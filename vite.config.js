c1ae5bf1f91e3558ed393f9ad3628acea18345d8
Mateus Belchior Almeida <mateus.belchior.98@gmail.com>
2025-09-09 10:39:32 -0300

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ export default {
     sourcemap: true,
     build: {
     chunkSizeWarningLimit: 600 * 1024, // Set the limit to 1000 KiB (1 MB)
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ol: ['ol'],
+          maptiler: ['@maptiler/sdk']
+        }
+      }
+    }
   },
   }
 }
