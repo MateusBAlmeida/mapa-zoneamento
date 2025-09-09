c184cd133cee960e2bb9a87da681157392e9ddec
Mateus Belchior Almeida <mateus.belchior.98@gmail.com>
2025-09-09 10:31:30 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ export default {
   build: {
     sourcemap: true,
     build: {
-    chunkSizeWarningLimit: 1000, // Set the limit to 1000 KiB (1 MB)
+    chunkSizeWarningLimit: 100000, // Set the limit to 1000 KiB (1 MB)
   },
   }
 }
